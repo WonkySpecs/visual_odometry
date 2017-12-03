84c72284faad771f130814ba685c69930e575854
Will Taylor <w1ll100@hotmail.co.uk>
2017-12-03 20:13:43 +0000

    Some useless crap

--- helper_functions.py
+++ helper_functions.py
@@ -9,6 +9,7 @@
 
 import numpy as np
 import cv2
+import math
 
 # Takes an image and a Hessian threshold value and
 # returns the SURF features points (kp) and descriptors (des) of image
@@ -38,9 +39,56 @@ def matchFeatures(des1, des2, number_of_checks, match_ratio):
             good_matches.append(m);
     return good_matches
 
-
 def computeEssentialMat(kp1, kp2, good_matches):
     #compute the transformation
     pts1 = np.float32([kp1[m.queryIdx].pt for m in good_matches]).reshape(-1,1,2);
     pts2 = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1,1,2);
     return cv2.findEssentialMat(pts1, pts2)
+
+def haversine_dist(lat1, long1, lat2, long2):
+    lat1    = math.radians(lat1)
+    long1   = math.radians(long1)
+    lat2    = math.radians(lat2)
+    long2   = math.radians(long2)
+
+    dlong = long2 - long1
+    dlat = lat2 - lat1
+
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlong / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    return 6371 * c * 1000
+
+def convert_gps_to_coords(gps_data):
+    lon, lat = gps_data[1], gps_data[2]
+
+    #54.767, -1.57002 - > 82, 410
+
+    return 5, 5
+
+#####################################################################
+
+# Code taken from the provided gyro.py file, which credited
+# lines 386 - 404 of yocto_gyro.py example
+# provided with YoctoLib.python 28878 (November 2017)
+# We are only interested in the heading
+
+def gyro_to_heading(orientation_x, orientation_y, orientation_z, orientation_w):
+    sqw = orientation_w * orientation_w;
+    sqx = orientation_x * orientation_x;
+    sqy = orientation_y * orientation_y;
+    sqz = orientation_z * orientation_z;
+    norm = sqx + sqy + sqz + sqw;
+    delta = orientation_y * orientation_w - orientation_x * orientation_z;
+
+    if delta > 0.499 * norm:
+            heading  = round(2.0 * 1800.0/math.pi * math.atan2(orientation_x,-orientation_w)) / 10.0;
+    else:
+            if delta < -0.499 * norm:
+                heading  = round(-2.0 * 1800.0/math.pi * math.atan2(orientation_x,-orientation_w)) / 10.0;
+            else:
+                heading  = round(1800.0/math.pi * math.atan2(2.0 * (orientation_x * orientation_y + orientation_z * orientation_w),sqw + sqx - sqy - sqz)) / 10.0;
+
+    return heading;
+
+#####################################################################
--- main.py
+++ main.py
@@ -6,24 +6,54 @@ import csv
 import time
 
 from helper_functions import *
+from gyro import gyro_to_angles
 
 dataset_path = os.path.join(os.curdir, "TTBB-durham-02-10-17-sub5")
 image_path = "left-images"
 
+#Get (noisy) ground truth gps data
+gps_data = []
+with open(os.path.join(dataset_path, "GPS.csv")) as GPS_file:
+	reader = csv.reader(GPS_file, delimiter=',')
+	first_line = True
+	for line in reader:
+		if not first_line:
+			gps_data.append([float(v) for v in line])
+		else:
+			first_line = False
+
+imu_data = []
+with open(os.path.join(dataset_path, "IMU.csv")) as GPS_file:
+	reader = csv.reader(GPS_file, delimiter=',')
+	first_line = True
+	for line in reader:
+		if not first_line:
+			imu_data.append([float(v) for v in line])
+		else:
+			col_names = line
+			first_line = False
+
+for i in range(5):
+	for a, b in zip(col_names, imu_data[i]):
+		print("{} : {}".format(a, b))
+	print("------\n")
+
 input_image_windowname = "Input image"
 output_track_windowname = "Trajectory"
+
 cv2.namedWindow(input_image_windowname, cv2.WINDOW_NORMAL)
-cv2.namedWindow(output_track_windowname, cv2.WINDOW_NORMAL)
+cv2.namedWindow(output_track_windowname)
 
-height = 1000
-width = 1000
-output_traj = np.zeros((height, width, 3), np.uint8)
-x = width / 2
-y = height / 2
+sat_img = cv2.imread("sat_img.png", cv2.IMREAD_COLOR)
+output_traj = cv2.resize(sat_img, None, fx = 0.5, fy = 0.5, interpolation = cv2.INTER_CUBIC)
+cv2.rectangle(output_traj, (3, 3), (430, 30), (0, 0, 0), -1)
+X_OFFSET = x = ground_x = 82
+Y_OFFSET = y = ground_y = 400
 
-for filename in os.listdir(os.path.join(dataset_path, image_path)):
+cur_gps_data = prev_gps_data = None
+
+for frame, filename in enumerate(os.listdir(os.path.join(dataset_path, image_path))):
 	if filename.endswith(".png"):
-		print(filename)
 		full_file_path = os.path.join(dataset_path, image_path, filename)
 		cur_img = cv2.imread(full_file_path, cv2.IMREAD_COLOR)
 
@@ -38,32 +68,59 @@ for filename in os.listdir(os.path.join(dataset_path, image_path)):
 
 			e_mat, mask = cv2.findEssentialMat(pts1, pts2)
 
-			inliers, R, t, mask = cv2.recoverPose(e_mat, pts1, pts2)
-
 			try:
-				total_t = total_t + (np.matmul(total_R, t))
-				total_R = np.matmul(R, total_R)
-			except NameError:
-				total_t = t
-				total_R = R
-				continue
+				inliers, R, t, mask = cv2.recoverPose(e_mat, pts1, pts2)
+			except:
+				print("recoverPose failed")
+				print(e_mat)
+				exit()
+
+			# cur_gps_data = gps_data[frame]
+			# cur_imu_data = imu_data[frame]
+			# if prev_gps_data:
+			# 	gps_dist = haversine_dist(cur_gps_data[1], cur_gps_data[2], prev_gps_data[1], prev_gps_data[2])
+			# 	gps_dist *= 0.8
+				
+			# 	heading = 140 - gyro_to_heading(cur_imu_data[1], cur_imu_data[2], cur_imu_data[3], cur_imu_data[4])
+			# 	heading = math.radians(heading)
+				
+			# 	ground_x += gps_dist * math.sin(heading)
+			# 	ground_y += gps_dist * math.cos(heading)
+
+			if math.fabs(t[0]) > math.fabs(t[1]) and math.fabs(t[0]) > math.fabs(t[2]):
+				try:
+					total_t = total_t + (np.matmul(total_R, t))
+					total_R = np.matmul(R, total_R)
+
+					x = total_t[0] + X_OFFSET
+					y = total_t[1] + Y_OFFSET
+
+					#cv2.circle(output_traj, (x, y), 2, (0, 0, 255))
+					cv2.rectangle(output_traj, (x - 1, y - 1), (x + 1, y + 1), (0, 0, 255), -1)
+
+					#cv2.rectangle(output_traj, (int(ground_x) - 1, int(ground_y) - 1), (int(ground_x) + 1, int(ground_y) + 1), (0, 255, 0), -1)
+				except NameError:
+					total_t = np.zeros((3, 1), np.float64)
+					total_R = R
+					continue
+			else:
+				print("Frame where forward is not primary movement ignored")
 
 			prev_img = cur_img
 			prev_kp, prev_des = cur_kp, cur_des
+			prev_gps_data = cur_gps_data
 
-			x = total_t[0] + width / 2
-			y = total_t[1] + height / 2
-
-			cv2.circle(output_traj, (x, y), 2, (0, 0, 255))
-
-			#cv2.drawKeypoints(cur_img, cur_kp, cur_img)
 			cv2.imshow(input_image_windowname, cur_img)
-			cv2.imshow(output_track_windowname, output_traj)
+			final_out = output_traj.copy()
+			cv2.putText(final_out, "x:{: 8.3f}    y:{: 8.3f}    z:{: 8.3f}".format(total_t[0][0], total_t[1][0], total_t[2][0]), (10, 22), cv2.FONT_HERSHEY_PLAIN, 1.25, (0, 0, 255), 2)
+			
+			cv2.imshow(output_track_windowname, final_out)
 
 		#Skip first frame, as no previous frame is known
 		except NameError:
 			prev_img = cur_img
 			prev_kp, prev_des = getFeatures(prev_img, 10000)
+			prev_gps_data = cur_gps_data
 			continue		
 
 	key = cv2.waitKey(40) & 0xFF
